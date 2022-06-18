a7e5a52212aa39177a0b15412fbfcdbe5b34fa7a
flatten-js <thetakahito@gmail.com>
2022-06-19 03:42:43 +0900

    three.js example

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,37 @@
-console.log('start')
+const THREE = require('three')
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+camera.position.z = 5
+
+function onResize() {
+  const { innerWidth: width, innerHeight: height, devicePixelRatio } = window
+  renderer.setPixelRatio(devicePixelRatio)
+  renderer.setSize(width, height)
+  camera.aspect = width / height
+  camera.updateProjectionMatrix()
+}
+
+onResize()
+window.addEventListener('resize', onResize)
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+
+  renderer.render(scene, camera)
+}
+
+animate()
