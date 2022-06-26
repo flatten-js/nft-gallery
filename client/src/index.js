1bd4ec48a3184f59a7a6245a4e4264da171b3251
flatten-js <thetakahito@gmail.com>
2022-06-27 00:50:48 +0900

    Scenes can now be loaded and moved freely

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,25 @@
 const THREE = require('three')
+const { GLTFLoader } = require('three/examples/jsm/loaders/GLTFLoader.js')
+const { PointerLockControls } = require('three/examples/jsm/controls/PointerLockControls.js')
+
+/*
+ *
+ */
 
 const scene = new THREE.Scene()
+scene.rotation.y = Math.PI
+scene.position.z = -32.5
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.y = 1.6
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
+const light = new THREE.AmbientLight(0xffffff)
+scene.add(light)
+
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.physicallyCorrectLights = true
 document.body.appendChild(renderer.domElement)
-
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-
-camera.position.z = 5
 
 function onResize() {
   const { innerWidth: width, innerHeight: height, devicePixelRatio } = window
@@ -23,13 +30,67 @@ function onResize() {
 }
 
 onResize()
+
 window.addEventListener('resize', onResize)
+
+/*
+ *
+ */
+
+// const { OrbitControls } = require('three/examples/jsm/controls/OrbitControls.js')
+
+// const axesHelper = new THREE.AxesHelper(5)
+// scene.add(axesHelper)
+//
+// const gridHelper = new THREE.GridHelper(100)
+// scene.add(gridHelper)
+//
+// const controls = new OrbitControls(camera, renderer.domElement)
+
+/*
+ *
+ */
+const loader = new GLTFLoader()
+loader.load('/assets/model/gallery.glb', glb => {
+  const model = glb.scene
+  scene.add(glb.scene)
+})
+
+const $overlay = document.getElementById('overlay')
+
+const controls = new PointerLockControls(camera, renderer.domElement)
+$overlay.addEventListener('click', () => controls.lock())
+controls.addEventListener('lock', () => $overlay.style.display = 'none')
+controls.addEventListener('unlock', () => $overlay.style.display = 'flex')
+
+const keys = { w: false, a: false, s: false, d: false }
+document.addEventListener('keydown', e => { keys[e.key] = true })
+document.addEventListener('keyup', e => { keys[e.key] = false })
+
+const direction = new THREE.Vector3()
+const velocity = new THREE.Vector3()
+
+/*
+ *
+ */
 
 function animate() {
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  if (controls.isLocked) {
+    const { w, a, s, d } = keys
+
+    velocity.z = 0
+    velocity.x = 0
+    direction.z = Number(w) - Number(s)
+    direction.x = Number(d) - Number(a)
+
+    if (w || s) velocity.z -= direction.z * 0.05
+    if (a || d) velocity.x -= direction.x * 0.05
+
+    controls.moveRight(-velocity.x)
+    controls.moveForward(-velocity.z)
+  }
 
   renderer.render(scene, camera)
 }
