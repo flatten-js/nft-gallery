8cdabad19f5cc5d953f793019e1079fbf8b76d10
flatten-js <thetakahito@gmail.com>
2022-07-17 13:38:50 +0900

    Added YAML for production environment

--- bin/start.bat
+++ bin/start.bat
@@ -1,8 +1,17 @@
 @echo off
 
+setlocal
+
+set prod=false
+if "%1" == "--prod" set prod=true
+if "%2" == "--prod" set prod=true
+if %prod% == true set f=-f docker-compose.prod.yml
+
 if "%1" == "update" (
-  docker-compose rm -fsv app
-  docker-compose up -d --build app
+  docker-compose %f% rm -fsv app
+  docker-compose %f% up -d --build app
 ) else (
-  docker-compose up -d --build
+  docker-compose %f% up -d --build
 )
+
+endlocal
